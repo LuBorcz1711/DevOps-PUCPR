9316118bfbc3cba4788b482d64244c7ba4f61737
LuBorcz1711 <luizabmachado17@gmail.com>
2026-03-28 18:48:05 -0300

    new tyr - Docker

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
 import random
+from fastapi import FastAPI
+from pydantic import BaseModal
 
 app = FastAPI()
+
+class Estudante(BaseModal):
+    name: str
+    curso: str
+    ativo: bool
 
 @app.get("/helloword")
 async def root():
@@ -10,3 +16,7 @@ async def root():
 @app.get("/funcaoteste")
 async def teste():
     return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante : Estudante):
+    return estudante
