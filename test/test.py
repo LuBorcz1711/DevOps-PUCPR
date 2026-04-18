5b5a031332cbdb0ef2133f3695cb60aae6a9743b
LuBorcz1711 <luizabmachado17@gmail.com>
2026-04-18 10:58:56 -0300

    commit - tests - 2nd try

--- test/test.py
+++ test/test.py
@@ -12,7 +12,7 @@ def test_funcaoteste():
 
 def test_create_estudante():
     estudante_test = Estudante(name="Lulu", curso="ADS", ativo=False)
-    assert estudante_test == create_estudante()
+    assert estudante_test == create_estudante(estudante_test)
 
 def test_update_estudante_negativo():
     assert not update_estudante(-5)
