b1d243c5bc9d423163e6f77ce6cffe68a387f8b0
LuBorcz1711 <luizabmachado17@gmail.com>
2026-04-18 11:26:25 -0300

    commit changes test.py

--- test/test.py
+++ test/test.py
@@ -2,26 +2,39 @@ from src.main import *
 from unittest.mock import patch
 
 def test_root():
-    assert root() == {"message": "Hello Word!"}
+    result = root()
+    yield result
+    assert result == {"message": "Hello Word!"}
 
 def test_funcaoteste():
     with patch('random.randint', return_value=123):
         result = teste()
+        yield result
 
     assert result == {"teste": True, "num_aleatorio": 123}
 
 def test_create_estudante():
     estudante_test = Estudante(name="Lulu", curso="ADS", ativo=False)
-    assert estudante_test == create_estudante(estudante_test)
+    result = create_estudante(estudante_test)
+    yield result
+    assert estudante_test == result
 
 def test_update_estudante_negativo():
-    assert not update_estudante(-5)
+    result = update_estudante(-5)
+    yield result
+    assert not result
 
 def test_update_estudante_positivo():
-    assert update_estudante(10)
+    result = update_estudante(10)
+    yield result
+    assert result
 
 def test_delete_estudante_negativo():
-    assert not delete_estudante(-5)
+    result = delete_estudante(-5)
+    yield result
+    assert not result
 
 def test_delete_estudante_positivo():
-    assert delete_estudante(10)
+    result = delete_estudante(10)
+    yield result
+    assert result
